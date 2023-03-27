0392932327152b7d02e47c39db2d51fc9d6ca3e3
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2023-03-27 00:11:29 -0500

    Capture video

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-import sys
+import datetime
+import time
+
 import cv2
 import zmq
 
 from image_helper import ImageHelper
-
 from object_detection import ObjectDetector
 from fps_counter import FpsCounter
 from label_diff import LabelDiff
@@ -14,8 +15,10 @@ class Server():
         self.init_zmq()
         self.init_model()
         self.init_utils()
+        self.init_cv()
         self.connected = False
         print("READY")
+
 
     def init_zmq(self):
         print("Initializing 0MQ")
@@ -34,7 +37,21 @@ class Server():
         print("Initializing Utilities")
         self.fps_counter = FpsCounter()
         self.label_diff = LabelDiff()
-        
+
+    def init_cv(self):
+        self.WINDOW_NAME = "Live Stream"
+        # start window thread for better performance
+        cv2.startWindowThread()
+
+        # create a window
+        cv2.namedWindow(self.WINDOW_NAME, cv2.WINDOW_NORMAL)
+
+        # set window to be on top of other windows
+        cv2.setWindowProperty(self.WINDOW_NAME, cv2.WND_PROP_TOPMOST, 1)
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video = cv2.VideoWriter('output.mp4', fourcc, 5, (640, 480))
+
     def execute(self):
         message = self.recv_with_timeout(30000 if not self.connected else 1000)
         self.connected = True
@@ -55,15 +72,40 @@ class Server():
         self.fps_counter.addFrame()
         # print(f'FPS: {self.fps_counter.getFps()}')
 
-        cv2.imshow('Image', bounded_image)
-        cv2.waitKey(1)
+        final_image = self.add_timestamp(bounded_image)
+        self.show_image(final_image)
+        self.video.write(final_image)
 
         # send confirmation to the client
         self.socket.send(b"Ready for next image")
 
+    def add_timestamp(self, image):
+        # get the current date and time
+        now = datetime.datetime.now()
+
+        # format the date and time as a string
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        # define the font and font scale
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.5
+
+        # define the position and color of the text
+        color = (255, 255, 255)
+        position = (10, 30)
+
+        # draw the text onto the image
+        cv2.putText(image, timestamp, position, font, fontScale, color, thickness=1)
+        return image
+
+    def show_image(self, image):
+        cv2.resizeWindow(self.WINDOW_NAME, image.shape[1], image.shape[0])
+        cv2.imshow(self.WINDOW_NAME, image)
+        cv2.waitKey(1)
+
     def recv_with_timeout(self, timeout = 10000):
         # Receive an image from RPi
-        socks = dict(self.poller.poll(timeout=10000))
+        socks = dict(self.poller.poll(timeout))
         if self.socket in socks and socks[self.socket] == zmq.POLLIN:
             return self.socket.recv()
         else:
@@ -74,3 +116,4 @@ class Server():
         self.socket.close()
         self.context.term()
         cv2.destroyAllWindows()
+        self.video.release()
